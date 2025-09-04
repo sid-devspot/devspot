a4b7b338dde30ae608add36605ca9c5b0373452b
sid-devspot <siddarthg.dev@gmail.com>
2025-09-04 12:38:18 +0530

    Backup Push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,27 +18,96 @@ import { BehaviorSubject } from 'rxjs';
 export class AppComponent implements OnInit {
   currecntComp: string = 'home';
   @ViewChildren('sectionRef') sections!: QueryList<ElementRef<HTMLElement>>;
-  @ViewChild('projectContainer', { static: false })
-  projectContainer!: ElementRef<HTMLDivElement>;
-  // isDarkMode = false;
-  // newTheme!: string;
   screenWidth: number = window.innerWidth;
   screenHeight: number = window.innerHeight;
 
-  constructor(private router: Router) {}
+  projects: any = {
+    walrec: [
+      {
+        img: './assets/Walrec/screenshot-01.png',
+        alt: 'Screenshot 01',
+        title: 'Login & Signup',
+        content: 'Basic username/password auth & Email',
+      },
+      {
+        img: './assets/Walrec/screenshot-02.png',
+        alt: 'Screenshot 02',
+        title: 'Dashboard',
+        content: 'Income & expenses in chart/calendar view',
+      },
+      {
+        img: './assets/Walrec/screenshot-03.png',
+        alt: 'Screenshot 03',
+        title: 'Expense & Income',
+        content: 'Add expenses with custom',
+      },
+      {
+        img: './assets/Walrec/screenshot-04.png',
+        alt: 'Screenshot 04',
+        title: 'Budget',
+        content: 'Track EMIs, subscriptions, etc.',
+      },
+      {
+        img: './assets/Walrec/screenshot-05.png',
+        alt: 'Screenshot 05',
+        title: 'Record',
+        content: 'Deatiled Record about Income and Expense',
+      },
+      {
+        img: './assets/Walrec/screenshot-06.png',
+        alt: 'Screenshot 06',
+        title: 'Profile',
+        content: 'Emoji-based avatar and user info',
+      },
+    ],
+    tarvellerSpond: [
+      {
+        img: './assets/Travellerspond/screenshot-01.png',
+        alt: 'Screenshot 01',
+        title: 'Home',
+        content: 'Highlights travel categories with eye-catching visuals',
+      },
+      {
+        img: './assets/Travellerspond/screenshot-02.png',
+        alt: 'Screenshot 02',
+        title: 'Tour Listings',
+        content: 'Detailed international & domestic packages',
+      },
+      {
+        img: './assets/Travellerspond/screenshot-03.png',
+        alt: 'Screenshot 03',
+        title: 'Tour Listings',
+        content: 'Detailed international & domestic packages',
+      },
+      {
+        img: './assets/Travellerspond/screenshot-04.png',
+        alt: 'Screenshot 04',
+        title: 'Contact',
+        content: 'Users can submit queries for quick follow-up',
+      },
+      {
+        img: './assets/Travellerspond/screenshot-05.png',
+        alt: 'Screenshot 05',
+        title: 'Admin Login',
+        content: 'Authenticated access for travel agency staff',
+      },
+      {
+        img: './assets/Travellerspond/screenshot-06.png',
+        alt: 'Screenshot 06',
+        title: 'Responsive Design',
+        content: 'Mobile-optimized and touch-friendly UI',
+      },
+    ],
+  };
 
-  ngOnInit() {
-    // const savedTheme = localStorage.getItem('theme') || 'dark-mode';
-    // this.isDarkMode = savedTheme === 'dark-mode';
-    // document.body.classList.add(savedTheme);
+  walrecCount = 0;
+  walrectCurrentProject: any = this.projects.walrec[this.walrecCount];
+  travellerCount = 0;
+  travellerCurrentProject: any = this.projects.tarvellerSpond[this.travellerCount];
 
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        const urlSegment = event.urlAfterRedirects.split('/').pop() || 'home';
-        this.currecntComp = urlSegment;
-      }
-    });
-  }
+  constructor() {}
+
+  ngOnInit() {}
 
   ngAfterViewInit(): void {
     const observer = new IntersectionObserver(
@@ -60,44 +129,45 @@ export class AppComponent implements OnInit {
     this.sections.forEach((section) => observer.observe(section.nativeElement));
   }
 
-  // Listener for screen resize
+  nextProject(param: string) {
+    if (param === 'walrec') {
+      if (this.walrecCount < this.projects.walrec.length - 1) {
+        this.walrecCount++;
+        this.walrectCurrentProject =
+          this.projects.walrec[this.walrecCount];
+      }
+    }
+
+    if (param === 'travellerspond') {
+      if (this.travellerCount < this.projects.tarvellerSpond.length - 1) {
+        this.travellerCount++;
+        this.travellerCurrentProject =
+          this.projects.tarvellerSpond[this.travellerCount];
+      }
+    }
+  }
+
+  previousProject(param: string) {
+    if (param === 'walrec') {
+      if (this.walrecCount > 0) {
+        this.walrecCount--;
+        this.walrectCurrentProject =
+          this.projects.walrec[this.walrecCount];
+      }
+    }
+
+    if (param === 'travellerspond') {
+      if (this.travellerCount > 0) {
+        this.travellerCount--;
+        this.travellerCurrentProject =
+          this.projects.tarvellerSpond[this.travellerCount];
+      }
+    }
+  }
+
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
     this.screenWidth = window.innerWidth;
     this.screenHeight = window.innerHeight;
-  }
-
-  // private theme = new BehaviorSubject<string>(
-  //   localStorage.getItem('theme') || 'dark-mode'
-  // );
-  // currentTheme = this.theme.asObservable();
-
-  // setTheme(theme: string): void {
-  //   const oldTheme = this.isDarkMode ? 'dark-mode' : 'light-mode';
-  //   document.body.classList.remove(oldTheme);
-  //   document.body.classList.add(theme);
-  //   this.isDarkMode = theme === 'dark-mode';
-
-  //   this.theme.next(theme);
-  //   localStorage.setItem('theme', theme);
-  // }
-
-  // toggleTheme(): void {
-  //   this.newTheme = this.isDarkMode ? 'light-mode' : 'dark-mode';
-  //   this.setTheme(this.newTheme);
-  // }
-
-  scrollLeft() {
-    this.projectContainer.nativeElement.scrollBy({
-      left: -400, // You can adjust how much to scroll
-      behavior: 'smooth',
-    });
-  }
-
-  scrollRight() {
-    this.projectContainer.nativeElement.scrollBy({
-      left: 400, // Positive value to scroll right
-      behavior: 'smooth',
-    });
   }
 }
